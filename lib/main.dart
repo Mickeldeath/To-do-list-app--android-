756a95e7133d2c7737c4875910041da075ca7733
Mickeldeath <160798530+Mickeldeath@users.noreply.github.com>
2024-04-12 12:44:36 -0400

    stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -18,3 +18,25 @@ void main() {
     ),
   );
 }
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text("To Do List"),
+          ),
+          backgroundColor: Colors.deepPurple[800],
+        ),
+        body: const Center(
+          child: Text("this is a try again"),
+        ),
+        backgroundColor: Colors.grey[800],
+      ),
+    );
+  }
+}
