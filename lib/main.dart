39dc11a1f53cb72c87dfe5621ac85f139915eaee
Mickeldeath <160798530+Mickeldeath@users.noreply.github.com>
2024-04-12 12:50:12 -0400

    added return how mywidget to the runapp

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Center(
-            child: Text("To Do List"),
-          ),
-          backgroundColor: Colors.deepPurple[800],
-        ),
-        body: const Center(
-          child: Text("this is a try again"),
-        ),
-        backgroundColor: Colors.grey[800],
-      ),
-    ),
-  );
+  runApp(MyWidget());
 }
 
 class MyWidget extends StatelessWidget {
