3c00d90b4eff588b03733e5f707b9c8686617802
Mickeldeath <160798530+Mickeldeath@users.noreply.github.com>
2024-04-12 18:15:06 -0400

    making box container and adding the possibility to click the checkbox

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todolistapp/utilities/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,10 +9,37 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //list of todo task
+  List toDoList = [
+    ["Do the dishes", false],
+    ["Do nothing", false],
+  ];
+
+  // checkbox was tapped
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.orange,
+    return Scaffold(
+      backgroundColor: Colors.orange[800],
+      appBar: AppBar(
+        title: Center(child: const Text('To Do List')),
+        elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+          );
+        },
+      ),
     );
   }
 }
